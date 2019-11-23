313f645a2b52ddb5b1b89c97872c561c79f0f2ab
Likeable <mark_vitez@yahoo.com>
2019-11-23 19:25:15 +0100

    update to the assistant code

--- Models/Admission.cs
+++ Models/Admission.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ServerAPI.Models
+namespace Models
 {
     public class Admission
     {
---
+++ ServerAPI/Controllers/DoctorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerAPI.Controllers
+{
+    [ApiController]
+    [Route("doctor")]
+    class DoctorController:ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Patient>> Get()
+        {
+            
+            return Ok();
+        }
+
+
+    }
+}
