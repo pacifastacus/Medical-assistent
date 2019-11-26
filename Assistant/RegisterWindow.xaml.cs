1db206f58658133b20244eabb015389c0e66f8f2
Palkovics Dénes - WinClient <pacifastacus@gmail.com>
2019-11-26 09:40:35 +0100

    lktsdm files are modified: -MainWindow.xaml: refactoring (AssistentWPF ->Assistan) -RegisterWindow.xaml: refactoring (AssistentWPF ->Assistan) -Assistant.csproj: VS automatic modifications
    Client project removed
    Doctor.Tests project removed
    DummyDB.cs from Models removed

--- Assistant/RegisterWindow.xaml.cs
+++ Assistant/RegisterWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace AssistentWPF
+namespace Assistant
 {
     /// <summary>
     /// Interaction logic for RegisterWindow.xaml
